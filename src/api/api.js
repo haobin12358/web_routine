35a2897f971d453b380eec82c026bead08551bd8
maoyaY <17774012962@163.com>
2019-06-27 13:15:59 +0800

    景区富文本显示

--- src/api/api.js
+++ src/api/api.js
@@ -12,6 +12,9 @@ const bind_phone = api2 +'user/bind_phone';//获取手机号
 const set_cost = api2 + 'play/set_cost';  //创建费用
 const set_insurance = api2 +'play/set_insurance'; //创建保险
 
+const scenicspot_list = api2 +'scenicspot/list';//获取景区list
+const scenicspot_get = api2 +'scenicspot/get';//获取景区详情
+
 
 
 
@@ -19,6 +22,6 @@ const set_insurance = api2 +'play/set_insurance'; //创建保险
 const img_src = 'https://play.bigxingxing.com/icon/';
 //评价
 module.exports = {
-  img_src,login,bind_phone,set_cost,set_insurance
+  img_src,login,bind_phone,set_cost,set_insurance,scenicspot_list,scenicspot_get
 
 }
