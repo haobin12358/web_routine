ce304697206545a9c17cbd390552ab1f8babc48c
maoyaY <17774012962@163.com>
2019-06-27 17:45:42 +0800

    发起活动接口调试

--- src/api/api.js
+++ src/api/api.js
@@ -10,7 +10,11 @@ const login = api2+'user/mp_login';//登录
 const bind_phone = api2 +'user/bind_phone';//获取手机号
 
 const set_cost = api2 + 'play/set_cost';  //创建费用
+const get_cost = api2 +'play/get_cost'; //获取费用
 const set_insurance = api2 +'play/set_insurance'; //创建保险
+const get_insurance = api2 +'play/get_insurance';//获取费用
+const set_play = api2 +'play/set_play';//创建活动
+const get_play = api2 +'play/get_play';//获取活动
 
 const scenicspot_list = api2 +'scenicspot/list';//获取景区list
 const scenicspot_get = api2 +'scenicspot/get';//获取景区详情
@@ -19,9 +23,10 @@ const scenicspot_get = api2 +'scenicspot/get';//获取景区详情
 
 
 
+
 const img_src = 'https://play.bigxingxing.com/icon/';
 //评价
 module.exports = {
-  img_src,login,bind_phone,set_cost,set_insurance,scenicspot_list,scenicspot_get
+  img_src,login,bind_phone,set_cost,set_insurance,scenicspot_list,scenicspot_get,set_play,get_cost,get_insurance,get_play
 
 }
