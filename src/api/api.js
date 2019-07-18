48b605b29e70e7ed52ddd958d4c185f22fe9d3b7
maoyaY <17774012962@163.com>
2019-07-18 17:06:31 +0800

    聊天详情页面，添加图片，视频，地图，地图选择，录音

--- src/api/api.js
+++ src/api/api.js
@@ -2,10 +2,12 @@
 
 const api2 = 'https://play.bigxingxing.com/api/v2/' ;
 // const api2 = 'https://pre2.bigxingxing.com/api/v2/';
+// const api2 = 'https://www.bigxingxing.com/api/v2/';
 
 
 const api = 'https://play.bigxingxing.com' ;
 // const api = 'https://pre2.bigxingxing.com';
+// const api = 'https://www.bigxingxing.com';
 //登录 注册
 
 const login = api2+'user/mp_login';//登录
@@ -76,6 +78,7 @@ const guide = api2 +'personalcenter/guide';//获取导游认证
 
 
 // const img_src = 'https://pre2.bigxingxing.com/icon/';
+// const img_src = 'https://www.bigxingxing.com/icon/';
 const img_src = '../../images/';
 //评价
 module.exports = {
